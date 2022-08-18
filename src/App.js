0eda846cb6d48844385a68a02264e19dd3d21e4a
Simon Nyholm Sørensen <simonnyholm@htomail>
2022-08-18 10:15:35 +0200

    randi 1

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       <h1>Welcome</h1>
       <Article
         title="Alle elsker jazz"
-        papragraph="Bandet er meget eftertragtet, og er især kendt for sit sceneoptræden, og har fået mange fine reviews og publikumsanerkendelse. Sekstetten består af nogle af Europas bedste jazzmusikere; hver især på deres unikke instrumenter. Bandet har et usædvanligt bredt repertoire af hele spektret af musik fra Jazzbyen: Traditional Jazz, Swing, Rythm & Blues, Latin, Gospel, Ragtime og nogle funky Brassband Street ig Mardi Grass Music."
+        paragraph="Bandet er meget eftertragtet, og er især kendt for sit sceneoptræden, og har fået mange fine reviews og publikumsanerkendelse. Sekstetten består af nogle af Europas bedste jazzmusikere; hver især på deres unikke instrumenter. Bandet har et usædvanligt bredt repertoire af hele spektret af musik fra Jazzbyen: Traditional Jazz, Swing, Rythm & Blues, Latin, Gospel, Ragtime og nogle funky Brassband Street ig Mardi Grass Music."
       />
     </div>
   );
