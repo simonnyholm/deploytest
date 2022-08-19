21ae808ee58e29e9f92bad9cf375cc20482bff42
Simon Nyholm Sørensen <simonnyholm@htomail>
2022-08-19 08:47:15 +0200

    add tailwind

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
 import Article from "./Article";
+import { motion } from "framer-motion";
 
 function App() {
+  const container = {
+    hidden: {
+      opacity: 0,
+    },
+    show: {
+      opacity: 1,
+      transition: {
+        delayChildren: 0.5,
+      },
+    },
+  };
+
+  const item = {
+    hidden: {
+      opacity: 0,
+      scale: 0.5,
+    },
+  };
   return (
     <div className="App">
-      <h1>Welcome</h1>
-      <Article
-        title="Alle elsker jazz"
-        paragraph="Bandet er meget eftertragtet, og er især kendt for sit sceneoptræden, og har fået mange fine reviews og publikumsanerkendelse. Sekstetten består af nogle af Europas bedste jazzmusikere; hver især på deres unikke instrumenter. Bandet har et usædvanligt bredt repertoire af hele spektret af musik fra Jazzbyen: Traditional Jazz, Swing, Rythm & Blues, Latin, Gospel, Ragtime og nogle funky Brassband Street ig Mardi Grass Music."
-      />
+      <motion.div
+        variants={container}
+        initial={{ scale: 0 }}
+        animate={{ scale: 1 }}
+        transition={{
+          ease: "easeInOut",
+          duration: 2,
+          delay: 2,
+          type: "spring",
+        }}
+      >
+        <h1>Welcome</h1>
+        <motion.section variants={item}>
+          {" "}
+          <Article
+            title="Alle elsker jazz"
+            paragraph="Bandet er meget eftertragtet, og er især kendt for sit sceneoptræden, og har fået mange fine reviews og publikumsanerkendelse. Sekstetten består af nogle af Europas bedste jazzmusikere; hver især på deres unikke instrumenter. Bandet har et usædvanligt bredt repertoire af hele spektret af musik fra Jazzbyen: Traditional Jazz, Swing, Rythm & Blues, Latin, Gospel, Ragtime og nogle funky Brassband Street ig Mardi Grass Music."
+          />
+        </motion.section>
+      </motion.div>
     </div>
   );
 }
---
+++ src/components/dialogBox.js
@@ -0,0 +1,10 @@
+const DialogBox = () => {
+    return ( <div>
+        <div>X</div>
+        <p>Du er nu tilmeldt vores service</p>
+        
+        <button>OK</button>
+    </div> );
+}
+ 
+export default DialogBox;
